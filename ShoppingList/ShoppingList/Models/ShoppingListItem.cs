fd23d9058d4da6f63257294fbf634e3b63629e84
John Tomlinson <johnt84@hotmail.co.uk>
2020-06-07 01:56:35 +0100

    Initial commit

---
+++ ShoppingList/ShoppingList/Models/ShoppingListItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShoppingList.Models
+{
+    public class ShoppingListItem
+    {
+        public string Filename { get; set; }
+        public string Text { get; set; }
+        public bool InTrolley { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}

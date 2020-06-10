ac8f434a8d2cd0b2e00e0d1ae13544c5defd76ce
John Tomlinson <johnt84@hotmail.co.uk>
2020-06-10 19:37:16 +0100

    Created Shopping List Blazor Web Assembly project

---
+++ ShoppingListBlazorWasm/Server/Controllers/ShoppingListItemController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingListBlazorWasm.Shared;
+using System;
+using System.Collections.Generic;
+
+namespace ShoppingListBlazorWasm.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ShoppingListItemController : ControllerBase
+    {
+        private List<ShoppingListItem> shoppingListItems;
+        private static DateTime shoppingDate = new DateTime(2020, 06, 11);
+
+        [HttpGet]
+        public List<ShoppingListItem> Get()
+        {
+            shoppingListItems = new List<ShoppingListItem>()
+            {
+                new ShoppingListItem()
+                {
+                    Name = "Bananas",
+                    Price = 0,
+                    Quantity = 1,
+                    ShoppingDate = shoppingDate,
+                    ItemPicked = false,
+                },
+                new ShoppingListItem()
+                {
+                    Name = "Tomatoes",
+                    Price = 0,
+                    Quantity = 1,
+                    ShoppingDate = shoppingDate,
+                    ItemPicked = false,
+                },
+                new ShoppingListItem()
+                {
+                    Name = "Lettuce",
+                    Price = 0,
+                    Quantity = 1,
+                    ShoppingDate = shoppingDate,
+                    ItemPicked = false
+                }
+            };
+
+            return shoppingListItems;
+        }
+    }
+}
---
+++ ShoppingListBlazorWasm/Shared/ShoppingListItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ShoppingListBlazorWasm.Shared
+{
+    public class ShoppingListItem
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public DateTime ShoppingDate { get; set; }
+        public bool ItemPicked { get; set; }
+    }
+}

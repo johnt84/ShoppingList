b00694ca6121232f159e6285972c23efe7a90f99
John Tomlinson <johnt84@hotmail.co.uk>
2020-06-11 18:56:45 +0100

    Added add, edit, display and delete pages

--- ShoppingListBlazorWasm/Server/Controllers/ShoppingListItemController.cs
+++ ShoppingListBlazorWasm/Server/Controllers/ShoppingListItemController.cs
@@ -2,6 +2,8 @@
 using ShoppingListBlazorWasm.Shared;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ShoppingListBlazorWasm.Server.Controllers
 {
@@ -9,41 +11,68 @@ namespace ShoppingListBlazorWasm.Server.Controllers
     [Route("[controller]")]
     public class ShoppingListItemController : ControllerBase
     {
-        private List<ShoppingListItem> shoppingListItems;
+        private List<ShoppingListItem> shoppingListItems = new List<ShoppingListItem>()
+        {
+            new ShoppingListItem()
+            {
+                Name = "Bananas",
+                Quantity = 1,
+                ShoppingDate = shoppingDate,
+            },
+            new ShoppingListItem()
+            {
+                Name = "Tomatoes",
+                Quantity = 1,
+                ShoppingDate = shoppingDate,
+            },
+            new ShoppingListItem()
+            {
+                Name = "Lettuce",
+                Quantity = 1,
+                ShoppingDate = shoppingDate,
+            },
+        };
         private static DateTime shoppingDate = new DateTime(2020, 06, 11);
+
 
         [HttpGet]
         public List<ShoppingListItem> Get()
         {
-            shoppingListItems = new List<ShoppingListItem>()
-            {
-                new ShoppingListItem()
-                {
-                    Name = "Bananas",
-                    Price = 0,
-                    Quantity = 1,
-                    ShoppingDate = shoppingDate,
-                    ItemPicked = false,
-                },
-                new ShoppingListItem()
-                {
-                    Name = "Tomatoes",
-                    Price = 0,
-                    Quantity = 1,
-                    ShoppingDate = shoppingDate,
-                    ItemPicked = false,
-                },
-                new ShoppingListItem()
-                {
-                    Name = "Lettuce",
-                    Price = 0,
-                    Quantity = 1,
-                    ShoppingDate = shoppingDate,
-                    ItemPicked = false
-                }
-            };
-
             return shoppingListItems;
+        }
+
+        public ShoppingListItem Get(string shoppingItemName)
+        {
+            return shoppingListItems.First(x => x.Name == shoppingItemName);
+        }
+
+        [HttpPost]
+        public IActionResult Create(ShoppingListItem shoppingListItem)
+        {
+            shoppingListItems.Add(shoppingListItem);
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Update(ShoppingListItem shoppingListItem)
+        {
+            var shoppingListItemToUpdate = Get(shoppingListItem.Name);
+
+            if (shoppingListItemToUpdate != null)
+            {
+                shoppingListItemToUpdate = shoppingListItem;
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(string shoppingItemName)
+        {
+            shoppingListItems.RemoveAll(x => x.Name == shoppingItemName);
+
+            return Ok();
         }
     }
 }
